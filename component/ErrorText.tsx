88c0ce2e40cab26924326c50ab622c0a1d725a4e
najmeh98 <foroughi.najmeh98@gmail.com>
2022-05-04 11:43:51 +0430

    add Error Text component

---
+++ component/ErrorText.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+import { useTheme } from "./Context/ThemeContext";
+
+export const ErrorText = ({ children }: { children: ReactNode }) => {
+  let t = useTheme();
+  return (
+    <Text
+      style={{
+        fontSize: t.fontSize.smaller,
+        fontFamily: t.fontFamily.MainFont,
+        color: t.color.errortext,
+        direction: t.direction,
+      }}
+    >
+      {children}
+    </Text>
+  );
+};
+
+const Text = styled.p`
+  width: 100%;
+  margin: 0px;
+`;
