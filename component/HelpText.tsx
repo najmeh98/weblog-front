d33140b263e26f039200f2a1938e8589629b7d3d
najmeh98 <foroughi.najmeh98@gmail.com>
2022-05-07 11:12:54 +0430

    add HelpText component

---
+++ component/HelpText.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+import { useTheme } from "./Context/ThemeContext";
+
+export const HelpText = ({
+  children,
+  onClick,
+}: {
+  children: ReactNode;
+  onClick: () => void;
+}) => {
+  let t = useTheme();
+  return (
+    <Text
+      style={{
+        fontSize: t.fontSize.normal,
+        fontFamily: t.fontFamily.MainFont,
+        color: t.color.borderColor,
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </Text>
+  );
+};
+
+const Text = styled.p`
+  padding: 0;
+  cursor: pointer;
+`;
