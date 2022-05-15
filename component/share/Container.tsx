1c4fad6068398a7d25f5483b0f3fc84e2b6c2c96
najmeh98 <foroughi.najmeh98@gmail.com>
2022-05-15 23:41:25 +0430

    add Style

---
+++ component/share/Container.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FormItem = styled.form`
+  display: flex;
+  flex-direction: column;
+  /* align-items: flex-start; */
+  justify-content: flex-start;
+
+  direction: rtl;
+`;
