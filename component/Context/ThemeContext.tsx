db1e7c78fccbb7feba8c68360ae60cdbfd1a5aa7
najmeh98 <foroughi.najmeh98@gmail.com>
2022-05-17 13:43:38 +0430

    add ThemeContext

---
+++ component/Context/ThemeContext.tsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { Theme } from "../../types/theme";
+import { lightTheme } from "../../utils/theme";
+
+type ContextType = Theme;
+
+const initialValue: ContextType = lightTheme;
+
+export const ThemeContext = React.createContext<ContextType>(initialValue);
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
