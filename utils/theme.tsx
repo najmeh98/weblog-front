fd28dc932940334543403da4abdc7eb996bac91e
najmeh98 <foroughi.najmeh98@gmail.com>
2022-05-18 15:22:09 +0430

    add useTheme

---
+++ utils/theme.tsx
@@ -0,0 +1,46 @@
+import { Theme } from "../types/theme";
+
+export const lightTheme: Theme = {
+  fontSize: {
+    smaller: 12,
+    small: 14,
+    normal: 16,
+    medium: 18,
+    semiLarge: 20,
+    large: 28,
+  },
+
+  fontFamily: {
+    MainFont: "",
+  },
+  fontWeight: {
+    regular: "400",
+    bold: "600",
+  },
+  margin: {
+    samll: 5,
+    medium: 10,
+    Large: 15,
+  },
+  height: 40,
+  width: "",
+  padding: {
+    small: 4,
+    normal: 8,
+    medium: 16,
+    fullpadding: "8px 16px",
+  },
+  color: {
+    fontColor: "#000",
+    bgColor: "#fff",
+    borderColor: "rgb(204, 204, 204)",
+    titleColor: "#7a7a7a",
+    buttonBg: "rgb(131, 123, 201)",
+    textColor: "#fff",
+    errortext: "#dd2020",
+  },
+  borderRadius: 10,
+  boxShadowbox: "0 1px 11px hsl(0deg 0% 66% / 27%)",
+  buttoncolor: "",
+  direction: "rtl",
+};
