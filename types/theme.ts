3cfe0e475da5495dbb46d522d01097690765bae4
najmeh98 <foroughi.najmeh98@gmail.com>
2022-05-18 15:21:33 +0430

    add typeof theme

---
+++ types/theme.ts
@@ -0,0 +1,40 @@
+export interface Theme {
+  fontSize: {
+    smaller: number;
+    small: number;
+    normal: number;
+    medium: number;
+    semiLarge: number;
+    large: number;
+  };
+  fontFamily: {
+    MainFont: string;
+  };
+  fontWeight: { regular: string; bold: string };
+  margin: {
+    samll: number;
+    medium: number;
+    Large: number;
+  };
+  height: number;
+  width: string;
+  padding: {
+    small: number;
+    normal: number;
+    medium: number;
+    fullpadding: string;
+  };
+  color: {
+    fontColor: string;
+    bgColor: string;
+    borderColor: string;
+    titleColor: string;
+    buttonBg: string;
+    textColor: string;
+    errortext: string;
+  };
+  borderRadius: number;
+  boxShadowbox: string;
+  buttoncolor: string;
+  direction: string;
+}
