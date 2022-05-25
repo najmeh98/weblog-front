62344c4a1f1cabdd3fa3ef2f268a295fd76e3c55
najmeh98 <foroughi.najmeh98@gmail.com>
2022-05-25 10:46:22 +0430

    create customfileInput component

---
+++ component/CustomFileInput.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { useTheme } from "./Context/ThemeContext";
+import { Props } from "./CustomInput";
+import { FormItem } from "./share/Container";
+import { Space } from "./share/Space";
+
+export const CustomFileInput = ({
+  label,
+  onChange,
+  type,
+  enctype,
+  placeholder,
+  name,
+}: Props) => {
+  let t = useTheme();
+
+  return (
+    <FormItem method="pots" encType={enctype} style={{ margin: "10px" }}>
+      {label && (
+        <>
+          <label style={{ color: t.color.titleColor }}>{label}</label>
+          <Space vertical={10} />
+        </>
+      )}
+      <Input
+        onChange={onChange}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        style={{ padding: t.padding.normal, height: t.height }}
+      />
+    </FormItem>
+  );
+};
+
+export const Input = styled.input`
+  outline: none;
+  border: 1px solid rgb(204, 204, 204);
+  direction: rtl !important;
+  display: flex;
+  text-align: right;
+  border-radius: 5px;
+  width: 100%;
+  margin-bottom: 10px;
+`;
