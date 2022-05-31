c692927dc3c5f6be3ff8e1e05e76c465779e8cd4
najmeh98 <foroughi.najmeh98@gmail.com>
2022-05-31 14:47:50 +0430

    add post request

--- pages/addPost.tsx
+++ pages/addPost.tsx
@@ -2,11 +2,12 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import React, { SetStateAction, useCallback, useState } from "react";
 import styled from "styled-components";
-import { config, createPost, SendPostrequest } from "../component/Api";
+import { config, createPost } from "../component/Api";
 import { useAppContext } from "../component/AppContext";
 import { useTheme } from "../component/Context/ThemeContext";
 import { CustomButton } from "../component/CustomButton";
 import { CustomDropdown } from "../component/CustomDropdown";
+import { CustomFileInput } from "../component/CustomFileInput";
 import { CustomInput } from "../component/CustomInput";
 import { CustomInputText } from "../component/CustomInputText";
 import { Category, Post } from "../component/definition";
@@ -21,117 +22,105 @@ export default function AddPost() {
   let t = useTheme();
   let { token, dispatch } = useAppContext();
 
-  const [formData, setFormData] = useState();
+  console.log(token);
+
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    file: "",
+  });
 
   const [category, setCategory] = useState();
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [loading, setLoading] = useState(false);
-  const [file, setFile] = useState<string>("");
+  const [file, setFile] = useState("");
   const [fileName, setFileName] = useState<string>("Choose File");
-  const [uploadedFile, setuploadedFile] = useState({});
+  const [uploadedFile, setuploadedFile] = useState([]);
 
   const uploadImageHandler = (e: any) => {
-    setFile(e.target.files[0]);
-    setFileName(e.target.files[0].name);
-    console.log(e.target.files[0].name);
+    console.log(e.target.files[0]);
+    setFormData({ ...formData, file: e.target.files[0] });
   };
 
   const CreatePostHandler = useCallback(async () => {
     setLoading(true);
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("content", content);
-    formData.append("file", file);
+    const data = new FormData();
+    data.append("title", formData.title);
+    data.append("content", formData.content);
+    data.append("file", formData.file);
     // formData.append("category", category);
 
-    console.log(file);
-    const post: Post = {
-      title,
-      content,
-      // category,
-      file,
-    };
-    // dispatch({
-    //   type: "post created",
-    //   post: post,
-    //   title: title,
-    //   content: content,
-    //   file: file,
-    //   category: category,
-    // });
-    try {
-      const res = await axios.post(
-        `${config.apiUrl}/api/data/add-post`,
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Accept: "application/json",
-            // token: token,
-          },
-        }
-      );
-      console.log(res);
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  }, [content, file, title]);
+    // console.log(formData.file);
+
+    console.log(token);
+
+    const res: any = await axios.post(
+      `${config.apiUrl}/api/data/add-post`,
+      data
+      //@ts-ignore
+      // { headers: { authorization: token } }
+    );
+    console.log(res);
+  }, [formData, token]);
 
   return (
-    <MainLayout>
-      <Wrapper
-        style={{ boxShadow: t.boxShadowbox, borderRadius: t.borderRadius }}
+    <MainLayout title="ایجاد پست جدید">
+      <Space vertical={10} />
+      <CustomInput
+        label="عنوان"
+        placeholder="عنوان..."
+        type="text"
+        value={formData.title}
+        onChange={(event: { target: { value: SetStateAction<string> } }) => {
+          setFormData({ ...formData, title: event.target.value });
+        }}
+      />
+      <Space vertical={5} />
+
+      <CustomInput
+        label="متن اصلی"
+        placeholder="متن اصلی"
+        type="textarea"
+        value={formData.content}
+        onChange={(event) =>
+          setFormData({ ...formData, content: event.target.value })
+        }
+      />
+      <Space vertical={10} />
+
+      <CustomInput
+        label="انتخاب تصویر"
+        type="file"
+        enctype="multipart/form-data"
+        name="file"
+        // value={fileName}
+        onChange={uploadImageHandler}
+      />
+      {/* <CustomFileInput
+        label="انتخاب تصویر"
+        type="file"
+        enctype="multipart/form-data"
+        name="file"
+        onChange={uploadImageHandler}
+        
+      /> */}
+
+      <Space vertical={3} />
+
+      <CustomDropdown
+        items={Category}
+        selectedItem={category}
+        setSelectedItem={setCategory}
+      />
+
+      <CustomButton
+        padding="10px 30px"
+        style={{ justifyContent: "flex-end", paddingLeft: "20px" }}
+        onClick={CreatePostHandler}
       >
-        <ThemedText fontSize="normal" fontWeight="bold">
-          ایجاد پست جدید
-        </ThemedText>
-        <Space vertical={10} />
-        <CustomInput
-          label="عنوان"
-          placeholder="عنوان..."
-          type="text"
-          value={title}
-          onChange={(event: { target: { value: SetStateAction<string> } }) => {
-            setTitle(event.target.value);
-            console.log(event.target.value);
-          }}
-        />
-        {/* <Space vertical={5} /> */}
-
-        <CustomInput
-          label="متن اصلی"
-          placeholder="متن اصلی"
-          type="textarea"
-          value={content}
-          onChange={(event) => setContent(event.target.value)}
-        />
-        <Space vertical={5} />
-
-        <CustomInput
-          label="انتخاب تصویر"
-          type="file"
-          enctype="multipart/form-data"
-          name="file"
-          // value={fileName}
-          onChange={uploadImageHandler}
-        />
-
-        <CustomDropdown
-          items={Category}
-          selectedItem={category}
-          setSelectedItem={setCategory}
-        />
-
-        <CustomButton
-          padding="10px 30px"
-          style={{ justifyContent: "flex-end", paddingLeft: "20px" }}
-          onClick={CreatePostHandler}
-        >
-          انتشار
-        </CustomButton>
-      </Wrapper>
+        انتشار
+      </CustomButton>
     </MainLayout>
   );
 }
