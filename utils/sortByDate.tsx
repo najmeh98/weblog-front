9baf306a94a481ea9827695034b3d57fc2c5d5f3
najmeh98 <foroughi.najmeh98@gmail.com>
2022-07-03 18:53:03 +0430

    sort by date

---
+++ utils/sortByDate.tsx
@@ -0,0 +1,9 @@
+import { Post } from "../component/definition";
+
+// export const sortByDate = (posts: Post, by: "created" | "updated") => {
+//   const sortedTasks = posts.sort((a: Post, b: Post) => {
+//     // if (by === "created") return +new Date(b.created) - +new Date(a.created);
+//   });
+// };
+
+const nowTime = new Date().toLocaleString();
